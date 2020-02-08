c0c51f2d67e3f2df143d589e123d30fb4cadbb8a
Praty21 <44113506+Praty21@users.noreply.github.com>
2020-02-08 19:50:03 +0530

    Updated Credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -1,9 +1,9 @@
-client_id=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/pratysin-97213-eval/apiproducts/Cicd-Prod-Product?query=list&entity=keys")
+client_id=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/pratysin-97213-eval/apiproducts/forecast?query=list&entity=keys")
 
 id=$(jq -r .[0] <<< "${client_id}" )
 echo $id
 
-client_secret=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/pratysin-97213-eval/developers/hr@api.com/apps/hrapp/keys/$id")
+client_secret=$(curl -H "Authorization: Basic $base64encoded" "https://api.enterprise.apigee.com/v1/organizations/pratysin-97213-eval/developers/pratysin@gmail.com/apps/forecastapp/keys/$id")
 
 secret=$(jq -r .consumerSecret <<< "${client_secret}" )
 echo $secret
